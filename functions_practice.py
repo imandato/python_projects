3f0d8e91c1757408348273552b3d468d995c5033
imandato <imandato211@gmail.com>
2022-11-19 09:26:47 -0500

    functions practice

---
+++ functions_practice.py
@@ -0,0 +1,22 @@
+def hello():
+    print("Hello user")
+
+hello()
+
+def pack(argument1, argument2, argument3):
+    return [argument1, argument2, argument3]
+
+print(pack("apples", "bananas", "pears"))
+
+def eat_lunch(lunch):
+    if len(lunch) == 0:
+        print("my lunchbox is empty")
+    else:
+        for i in range(len(lunch)):
+            if i == 0:
+                print(f"First I eat {lunch[0]}")
+            else:
+                print(f"Next I eat {lunch[i]}")
+    
+
+print(eat_lunch(["pears", "apples", "oranges"]))
